d36f4da8097f7b54293d22f3b9b999be5d913fda
James Petersen <james@jamespetersen.com>
2019-05-15 19:18:04 -0700

    adding icon

--- lovelace-countdown-dates.js
+++ lovelace-countdown-dates.js
@@ -35,8 +35,8 @@ class CountdownCard extends HTMLElement {
       
       line += `
               <div style="padding: 5px;">
-                <ha-icon icon="${element.icon ? element.icon : ""}"></ha-icon>
-                <div class="countdown-name" style="float:left; width:65%">${element.name}</div>
+                
+                <div class="countdown-name" style="float:left; width:65%"><ha-icon icon="${element.icon ? element.icon : ""}"></ha-icon> ${element.name}</div>
                 <div class="countdown-date" style="text-align: right;">${daysLeft} ${this.phrase}</div>
               </div>
               `;
